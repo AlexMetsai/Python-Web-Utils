3353ae494c7fbba2d08fafb0cbdcf44a6e8c3d25
Alexandros Metsai <alexmetsai@gmail.com>
2021-07-05 11:58:55 +0300

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,9 @@
-# TODO
+# Alexandros I. Metsai
+# alexmetsai@gmail.com
+
+import requests
+import xmltodict
+
+def load_xml_from_url(url):
+    response = requests.get(url)
+    return xmltodict.parse(response.content)
