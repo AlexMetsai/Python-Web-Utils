f49d01d249a631fed1461ab0f36ce5f6a5e6cbb3
Alexandros Metsai <alexmetsai@gmail.com>
2021-07-05 13:15:10 +0300

    added ElementTree functionality and doc

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,22 @@
 
 import requests
 import xmltodict
+import xml.etree.ElementTree as ET
 
 def xml_to_dict_from_url(url):
+    """
+    Return ordered dict with XML contents.
+    
+    :param url: The url of the xml document.
+    """
     response = requests.get(url)
     return xmltodict.parse(response.content)
+
+def xml_tree_from_url(url):
+    """
+    Return root using the Element Tree XML API.
+    
+    :param url: The url of the xml document.
+    """
+    response = requests.get(url)
+    return ET.fromstring(response.content)
