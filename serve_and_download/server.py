39f4768923e846449efd92d21b5bcdd3e16bc683
Alexandros Metsai <alexmetsai@gmail.com>
2021-07-08 12:22:00 +0300

    Update server.py

--- serve_and_download/server.py
+++ serve_and_download/server.py
@@ -1,1 +1,13 @@
-#TODO
+# Alexandros I. Metsai
+# Serve a file over HTTP
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), handler) as httpd:
+    print("Server started at localhost:" + str(PORT))
+    httpd.serve_forever()
