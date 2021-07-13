37dfee33eeebebe28a6123fc4be0e8363ddc73ad
Alexandros Metsai <alexmetsai@gmail.com>
2021-07-13 09:22:44 +0300

    fernet encryptor

--- utils/cryptography_utils.py
+++ utils/cryptography_utils.py
@@ -8,3 +8,15 @@ from cryptography.fernet import Fernet
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
+
+def fernet_encryptor(message, key):
+    """
+    Fernet symmetric key cipher.
+
+    :param message: The message to be encrypted
+    :param key: Encryption key
+    :return: Encrypted message
+    """
+    fernet = Fernet(key)
+    enc_message = fernet.encrypt(message.encode())
+    return enc_message
