c31e3a20ff3236047c3de21765a454c6199fd134
Alexandros Metsai <alexmetsai@gmail.com>
2021-07-21 17:40:01 +0300

    request handler constructor

--- serve_and_download_with_authentication/server.py
+++ serve_and_download_with_authentication/server.py
@@ -5,3 +5,13 @@ from functools import partial
 from http.server import SimpleHTTPRequestHandler#, test
 import base64
 import os
+
+
+class AuthHTTPRequestHandler(SimpleHTTPRequestHandler):
+    """ Main class to present webpages and authentication. """
+
+    def __init__(self, *args, **kwargs):
+        username = kwargs.pop("username")
+        password = kwargs.pop("password")
+        self._auth = base64.b64encode(f"{username}:{password}".encode()).decode()
+        super().__init__(*args)
