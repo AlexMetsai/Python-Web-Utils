cf1b5555d4c4c2c97e336f5f0378c314367411bd
Alexandros Metsai <alexmetsai@gmail.com>
2021-08-25 12:24:38 +0300

    Update downloader.py

--- serve_and_download_with_authentication/downloader.py
+++ serve_and_download_with_authentication/downloader.py
@@ -1,1 +1,10 @@
+# Alexandros I. Metsai
+# Download a file over http.
 
+import requests
+
+
+url = 'http://localhost:8000/empty.csv'
+r = requests.get(url)
+print(f"Reading file from {url}")
+open('../new_file.csv', 'wb').write(r.content)
